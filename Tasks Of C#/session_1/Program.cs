7896216e3583ab28f168f8ca5541c2f1066b3818
Mahmoud Ayed Bayomi <137709468+mahmoud-aied@users.noreply.github.com>
2023-06-27 18:56:44 +0300

    finish task-1

---
+++ Tasks Of C#/session_1/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Session_1_Task
+{
+    internal class TestCode
+    {
+
+
+        static void Main(string[] args)
+        {
+            #region Questions
+            /*
+                1- write a C#  program to calculate the sum and the average of three numbers.
+
+                2- Write a C#  program that asks the user to enter the radius of a circle and 
+                   then computes and displays the circle's  area and circumference.
+
+                3- Write a C# program that accepts an employee salary and a tax percentage 
+                   then computes the Net Salary and Annual Salary. 
+            */
+            #endregion
+
+            #region Question 1
+            //double Sum = 0;
+            //double Avg = 0;
+            //Console.WriteLine("Enter The Three Numbers : ");
+            //var num1 = double.Parse(Console.ReadLine());
+            //var num2 = double.Parse(Console.ReadLine());
+            //var num3 = double.Parse(Console.ReadLine());
+            //Sum = num1 + num2 + num3;
+            //Avg = (Sum) / 3;
+            //Console.WriteLine($"The Sum Of Numbers =   {Sum}");
+            //Console.WriteLine($"The Average Of Numbers =  {Avg}");
+            #endregion
+
+            #region Question 2
+            //const double I = 3.14;
+            //double Area = 0;
+            //double Circumference = 0;
+
+            //Console.Write("Radius = ");
+            //double R = double.Parse(Console.ReadLine());
+
+            //Area = I * R * R;
+            //Circumference = 2 * I * R;
+
+            //Console.WriteLine($"Area Of Circle = {Area} ");
+            //Console.WriteLine($"Circumference Of Circle  =  {Circumference} ");
+            #endregion
+
+            #region Question 3
+            //Console.Write("The Salary Is : ");
+            //double Salary = double.Parse(Console.ReadLine());
+            //Console.Write("The Tax Percentage = ");
+            //double TaxPercentage = double.Parse(Console.ReadLine());
+            //double NetSalary = Salary * (100 - TaxPercentage) / 100;
+            //double AnlSalary = NetSalary * 12;
+            //Console.WriteLine($"Net Salary = {NetSalary} ");
+            //Console.WriteLine($"Annual Salary = {AnlSalary}");
+            #endregion
+
+        }
+    }
+}
